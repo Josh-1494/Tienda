725ecea5636f7bd5508810516cc220233e7a0d75
jovie <jovie@LAPTOP-4KFI00NB>
2022-10-10 23:38:35 -0600

    Clase semana 04

---
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -0,0 +1,30 @@
+
+package com.tienda.controller;
+
+import com.tienda.domain.Cliente;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    
+    @GetMapping("/")
+    public String inicio(Model model){
+        
+        //Este tiene que ser un var en vez del String
+        var texto="Estamos en semana 4";
+        model.addAttribute("mensaje",texto);
+        
+        Cliente cliente1 = new Cliente("Juan","Perez Moreno","jperez@gmail.com","2222-8888");
+        Cliente cliente2 = new Cliente("Joshua","Oviedo Arias","joviedo@gmail.com","1234-5678");
+        Cliente cliente3 = new Cliente("Maria","Solis Arguedas","msolis@gmail.com","7894-1235");
+        
+        var clientes = Arrays.asList(cliente1,cliente2,cliente3); 
+        
+        model.addAttribute("clientes",clientes);
+        
+        return "index";
+    }
+    
+}
