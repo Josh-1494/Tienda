20896e2845b73330d3f76c10f0425bf3aeb9a915
jovie <jovie@LAPTOP-4KFI00NB>
2022-10-16 17:58:03 -0600

    Clase semana 04 - Primera parte al dia

--- src/main/java/com/tienda/controller/IndexController.java
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -13,17 +13,23 @@ public class IndexController {
     @GetMapping("/")
     public String inicio(Model model){
         
-        //Este tiene que ser un var en vez del String
         var texto="Estamos en semana 4";
         
         model.addAttribute("mensaje",texto);
         
+        
+         Cliente cliente = new Cliente("Juan","Perez Moreno","jperez@gmail.com","2222-8888");
+         model.addAttribute("cliente",cliente);
+        
+         
         Cliente cliente1 = new Cliente("Juan","Perez Moreno","jperez@gmail.com","2222-8888");
         Cliente cliente2 = new Cliente("Joshua","Oviedo Arias","joviedo@gmail.com","1234-5678");
         Cliente cliente3 = new Cliente("Maria","Solis Arguedas","msolis@gmail.com","7894-1235");
         
         var clientes = Arrays.asList(cliente1,cliente2,cliente3); 
         
+                           /*ESTE ES EL NOMBRE CON EL QUE SE LLAMA A LA LISTA*/
+                                    /*     ▼     */
         model.addAttribute("clientes",clientes);
         
         return "index";
